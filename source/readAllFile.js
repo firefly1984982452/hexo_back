b95dbed0dd5270d0d91f6f2f2ca2f740ddca678a
pengdan <1982982452@adgroup.tslsmart.com>
2021-07-13 10:25:14 +0800

    1

--- source/readAllFile.js
+++ source/readAllFile.js
@@ -1,11 +1,11 @@
 const fs = require("fs");
-
 const dir = "./_posts";
+
 const files = fs.readdirSync(dir);
 
-let arr = [];
-let filesList = files.filter((v) => v.includes("《"));
-for (file of filesList) {
-  arr.push(file);
-}
-console.log(arr);
+// let arr = [];
+// let filesList = files.filter((v) => v.includes("《"));
+// for (file of filesList) {
+//   arr.push(file);
+// }
+console.log(JSON.stringify(files));
